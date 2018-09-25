fc28904e339873d544df22761471f5b0cfb7b342
az9222 <angelazhou922@gmail.com>
2018-09-24 18:25:00 -0700

    fix setTimeout and BlinkyDancer.prototype.step

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,26 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+var BlinkyDancer = function(top, left, timeBetweenSteps) {
+  Dancer.call(this, top, left, timeBetweenSteps);
+  
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 
-  var oldStep = blinkyDancer.step;
-
-  blinkyDancer.step = function() {
-    // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
-  };
-
-  return blinkyDancer;
 };
+
+BlinkyDancer.prototype = Object.create(Dancer.prototype);
+
+BlinkyDancer.prototype.constructor = BlinkyDancer;
+
+BlinkyDancer.prototype.step = function() {
+  console.log(this);
+  var oldStep = this.step; //TODO
+  oldStep();
+  this.$node.toggle();
+}
+
+/*
+- Create at least two new dancers
+- Add a link in the top bar of dancefloor.html to create a dancer of each new type you create
+- Write tests for new types. Use spec/blinkyDancerSpec.js as a template for new test files, and make sure to include them in SpecRunner.html
+*/
+
