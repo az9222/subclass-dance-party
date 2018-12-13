7c4e3da8171f55296b71ee685c233e8b4aef4128
Angela Zhou <angelazhou922@gmail.com>
2018-12-12 23:04:34 -0800

    solutions

--- src/dancer.js
+++ src/dancer.js
@@ -1,42 +1,82 @@
+
+/**
+ * ES6 Solution explained
+ *
+ * JavaScript’s global object (window in web browsers, global in Node.js)
+ * is more a bug than a feature, especially with regard to performance.
+ * That’s why it’s not surprising that ES6 introduces a distinction:
+ *
+ * All properties of the global object are global variables.
+ * In global scope, the following declarations create such properties:
+ *   - `var` declarations
+ *   - Function declarations
+ *
+ * But there are now also global variables that are not properties of the global object.
+ * In global scope, the following declarations create such variables:
+ *   - `let` declarations
+ *   - `const` declarations
+ *   - Class declarations
+ *
+ * Source: http://www.2ality.com/2015/02/es6-scoping.html
+ *
+ * In this sprint, we use the `window` object to dynamically select the
+ * constructor function to be instantiated. Because of the rule above,
+ * we have to use an anonymous class expression with the `var` keyword
+ */
+
+// var Dancer = class {
+//   constructor(top, left, timeBetweenSteps) {
+//     this.$node = $('<span class="dancer"></span>');
+//     this._timeBetweenSteps = timeBetweenSteps;
+//     this.step();
+//     this.setPosition(top,left);
+//   }
+
+//   step() {
+//     // Arrow function acknowledges lexical `this`
+//     setTimeout(() => this.step(), this._timeBetweenSteps)
+//   }
+
+//   setPosition(top, left) {
+//     var styleSettings = {
+//       top: top,
+//       left: left
+//     };
+//     this.$node.css(styleSettings);
+//   }
+// }
+
+// ES5 Solution
 var Dancer = function(top, left, timeBetweenSteps) {
-    this.timeBetweenSteps = timeBetweenSteps;
-    // use jQuery to create an HTML <span> tag
-    this.$node = $('<span class="dancer"></span>');
-    this.step();
-    this.setPosition(top, left);
-}
-
-Dancer.prototype.step = function() {
-    // the basic dancer doesn't do anything interesting at all on each step,
-    // it just schedules the next step
-    setTimeout(this.step.bind(this), this.timeBetweenSteps);
-    // this.step.bind(this) used to be dancer.step and we want to ensure that when we call it, 
-    // it is with the Dancer so we want to make sure that the context of this is clear
-}; 
-    
-
-Dancer.prototype.constructor = Dancer;
-
-Dancer.prototype.setPosition = function(top, left) {
-    // Use css top and left properties to position our <span> tag
-    // where it belongs on the page. See http://api.jquery.com/css/
-    var styleSettings = {
-        top,
-        left
-    };
-    this.$node.css(styleSettings);
+  this.$node = $('<span class="dancer"></span>');
+  this._timeBetweenSteps = timeBetweenSteps;
+  this.step();
+  this.setPosition(top, left);
 };
 
-Dancer.prototype.transform = function(x, y) {
-    var origLeft = parseInt(this.$node.css("left"));
-    var origTop = parseInt(this.$node.css("top"));
-    this.$node.animate({left: origLeft + x, top: origTop + y}, 1000);
-}
+Dancer.prototype.step = function() {
+  // There are two implementations below: one that requires an extra
+  // clock tick in the test, and one that does not. In blinkyDanceSpec.js,
+  // notice there are two back-to-back invocations of:
+  //
+  //   clock.tick(timeBetweenSteps);
+  //   clock.tick(timeBetweenSteps);
+  //
+  // Depending on which approach you take, this extra clock tick may
+  // be necessary.
 
-Dancer.prototype.moveTo = function(x, y) {
-    this.$node.animate({left: x, top: y}, 500);
-}
+  // v1: requires an extra clock tick
+  // setTimeout(this.step.bind(this), this._timeBetweenSteps);
 
+  // v2: does not require extra clock tick
+  var context = this;
+  setTimeout(function() { context.step(); }, this._timeBetweenSteps);
+};
 
-    // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
-    // this one sets the position to some random default point within the body
+Dancer.prototype.setPosition = function(top, left) {
+  var styleSettings = {
+    top: top,
+    left: left
+  };
+  this.$node.css(styleSettings);
+};
--- src/init.js
+++ src/init.js
@@ -15,47 +15,20 @@ $(document).ready(function() {
      * A new object of the given type will be created and added
      * to the stage.
      */
-
-     //grabs data-dancer-maker-function-name = "BlinkyDancer"
-    var dancerMakerFunctionName = $(this).data('dancer-maker-function-name'); //
-    // var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
-    // grabs value of data-dancer-maker-function-name="BlinkyDancer"
-    // dancerMakerFunctionName = "BlinkyDancer"
+    var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
 
     // get the maker function for the kind of dancer we're supposed to make
-    var dancerMakerFunction = window[dancerMakerFunctionName]; //so window looks for dancerMakerFunctionName and finds the BlinkyDancer function which makes a dancer obj
+    var dancerMakerFunction = window[dancerMakerFunctionName];
 
-    var height = $("body").height();
-    var width = $("body").width();
-
-    function randomRange(min, max) {
-      return Math.random() * (max - min) + min;
-    }
     // make a dancer with a random position
+
+
     var dancer = new dancerMakerFunction(
-      height * randomRange(0.6, 0.8),
-      width * Math.random(),
+      $('body').height() * Math.random(),
+      $('body').width() * Math.random(),
       Math.random() * 1000
     );
-    $('body').append(dancer.$node);
-    window.dancers.push(dancer);
+        $('body').append(dancer.$node);
   });
-
-  $('.addLineUpButton').on('click', function(event) {
-    var height = $("body").height();
-    var width = $("body").width();
-    var spaceBetween = width / window.dancers.length;
-
-
-    for (var d = 0; d < window.dancers.length; d++) {
-        // window.dancers[d].moveTo(d * spaceBetween, height * 0.9);
-        dancers[d].$node.stop();
-        dancers[d].$node.css({
-            "left" : d * spaceBetween,
-            "top" : height * 0.9
-        });
-    }
-
-  });
-
 });
+
